f2bc137a83d088b4cd162957224c7ce0158009c1
Seth Deal <37190139+Seth-Deal@users.noreply.github.com>
2019-09-10 22:05:28 -0400

    add aliases

---
+++ aliases.sh
@@ -0,0 +1,11 @@
+# dope rsync version of move with progress
+alias mooove='rsync -Pa --remove-source-files'
+
+# some more ls aliases
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+
+# for shits and giggles
+alias fucking='sudo apt update && sudo apt install -y'
+alias please='sudo'
