d0e733e323574599e72404cd298ba92c52c9ce62
Seth Deal <skdeal3@gmail.com>
2019-12-30 20:19:47 -0500

    New ll

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -2,10 +2,13 @@
 alias mooove='rsync -Pa --remove-source-files'
 
 # some more ls aliases
-alias ll='ls -alF'
-alias la='ls -A'
-alias l='ls -CF'
+alias grep='grep --color=auto'
+alias k=kubectl
+alias ll="ls -lhF"
+alias lll="ll -a"
+alias ls="ls -GF"
 
 # for shits and giggles
 alias fucking='sudo apt update && sudo apt install -y'
 alias please='sudo'
+alias table-flip='echo "（╯°□°）╯︵ ┻━┻" | pbcopy'
